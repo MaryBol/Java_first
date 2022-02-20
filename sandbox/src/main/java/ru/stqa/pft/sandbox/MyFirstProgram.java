e3e33b273dc998897b87f41ec929b7849c14bfe5
MariBol <shvanovam@gmail.com>
2022-02-20 23:39:10 +0300

    Exercise create class Point for two points on flat, create function public static double distance(Point p1, Point p2).

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -11,9 +11,22 @@ public class MyFirstProgram {
 
     Rectangle r = new Rectangle(4,6);
        System.out.println("Площадь прямоуольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
-  }
+
+       Point p1=new Point();
+       p1.x=2;
+       p1.y=3;
+       Point p2=new Point();
+      p2.x=4;
+      p2.y=2;
+       System.out.println("расстояние между двумя точками координат " + "p1("+ p1.x + "," +p1.y+")"+ " и "+"p2("+p2.x+","+p2.y+")"+" равно "+distance(p1,p2));
+
+}
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
   }
 }
